d9a4ed98047cf4e66d0fbaf8783d13890ac6cbb6
nextcodecamp <nextcodecamp@gmail.com>
2023-06-21 12:00:43 +0700

    Update zigzag.py
    Turtle walk by nextcodecamp.
    For computational thinking book.

--- zigzag.py
+++ zigzag.py
@@ -4,33 +4,27 @@
 # .  By Nextcodecamp.       #
 ############################
 print("Turtle by Nextcodecamp")
-vegetable = 4
+vegetable = 5
 posx = 2
 posy = 2
-posveg = [[4, 3, "broccoli"], [8, 3, "carrot"], [8, 5, "carrot"], [2, 6, "lettuce"], [5, 7, "broccoli"]]
-
+posveg = [[4, 3, "broccoli"], [8, 3, "carrot"], [8, 5, "carrot"],
+          [2, 6, "lettuce"], [5, 7, "broccoli"]]
 broccoli = 0
 carrot = 0
 lettuce = 0
 
 
-def posTurtle():
-    turtlepos = {"posx": posx, "posy": posy}
-    return turtlepos
-
 def turtleWalk():
     global vegetable
     global posx, posy, broccoli, carrot, lettuce
+    direction = "RIGHT"
     while (vegetable != 0 or (posx == 9 and posy == 9)):
-        print("move to loc:", posx, posy)  # current position
-        if (posx != 9):
-            posx = posx + 1
-        if (posx == 9):  # reach last cell in row, go to the next row
-            posx = 1
-            posy = posy + 1
+        print("current position:", posx, posy)  # current position
         for x in posveg:  # check vegetable
             if (posx == x[0] and posy == x[1]):
                 vegetable = vegetable - 1
+                if (vegetable == 0):  #we need it to break from loop without  doing any action
+                    break
                 if (x[2] == "broccoli"):
                     print("grab broccoli at ", posx, posy)
                     broccoli = broccoli + 1
@@ -41,8 +35,20 @@ def turtleWalk():
                     print("grab lettuce at ", posx, posy)
                     lettuce = lettuce + 1
 
-
-print("Current position:", posTurtle())
+        if (direction == "RIGHT"):
+            posx = posx + 1
+            print("RIGHT 1 step to ", posx, posy)
+        if (direction == "LEFT"):
+            posx = posx - 1
+            print("LEFT 1 step to ", posx, posy)
+        if (posx == 9 or posx == 1):   #check if reach the last/first cell
+            posy = posy + 1
+            print("MOVE")    #change row
+            if (posx == 9):  #change direction
+                direction = "LEFT"
+            else:
+                direction = "RIGHT"
+print("Start position (x,y):", posx,posy)
 turtleWalk()
 if (vegetable == 0):
     print("I am full.")
